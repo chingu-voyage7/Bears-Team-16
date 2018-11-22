2bc22991a334b85871e3f0562865774586ffad41
Barry Carter <chingu@barrycarter.info>
2018-11-22 14:31:46 -0700

    Test w/ actually running server

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
-var express = require('express');
-var app = express();
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+const http = require('http');
+
+let app = http.createServer((req, res) => {  
+    // Set a response type of plain text for the response
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+
+    // Send back a response and end the connection
+    res.end('Hello World!\n');
   });
+
+
+app.listen(3000, '127.0.0.1');  
+console.log('Node server running on port 3000');  
+
+// var express = require('express');
+// var app = express();
+// app.get('/', function (req, res) {
+//     res.send('Hello World!');
+//  });
 
 // while (true) {}
 
