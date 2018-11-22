7226b665460bde23520af2025e731cc6929f4197
Barry Carter <chingu@barrycarter.info>
2018-11-22 14:51:02 -0700

    More testing.

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
-const http = require('http');
+const express = require('express')
+  const path = require('path')
+const PORT = process.env.PORT || 5000
 
-let app = http.createServer((req, res) => {  
+  express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+// const http = require('http');
+
+// let app = http.createServer((req, res) => {  
     // Set a response type of plain text for the response
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+//    res.writeHead(200, {'Content-Type': 'text/plain'});
 
     // Send back a response and end the connection
-    res.end('Hello World!\n');
-  });
+//    res.end('Hello World!\n');
+//  });
 
 
 app.listen(3000, '127.0.0.1');  
