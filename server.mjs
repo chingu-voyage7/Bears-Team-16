62c75fe290f8b328c747753b484ee87edf3a3e55
RefinedSoftwareLLC <mikar_gibbros@yahoo.com>
2018-11-27 17:24:13 -0700

    switched to es6 modules

--- server.mjs
+++ server.mjs
@@ -1,11 +1,11 @@
-require('ejs');
-const express = require('express');
-const path = require('path');
+import 'ejs';
+import express from 'express';
+import path from 'path';
 const PORT = process.env.PORT || 5000;
 
   express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
+  .use(express.static(path.join(path.resolve(), 'public')))
+  .set('views', path.join(path.resolve(), 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
