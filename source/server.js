5021b55d3b338821dd7bcae0057980d519937384
RefinedSoftwareLLC <mikar_gibbros@yahoo.com>
2018-12-16 15:31:59 -0700

    npm server

--- source/server.js
+++ source/server.js
@@ -1,1 +1,8 @@
-console.log('hello world');
+import path from 'path';
+import express from 'express';
+let app = express();
+
+app.use(express.static('.'));
+// app.use('/', express.static(path.join(__dirname, 'public')))
+
+console.log('Webpage at http://localhost:3000');
